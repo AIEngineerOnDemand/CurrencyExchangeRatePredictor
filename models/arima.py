95895bbf77eaf934bbcc18bf1e178366c649798c
AIEngineerOnDemand <giopantana@gmail.com>
2024-03-28 14:52:58 -0500

    Add ARIMA model implementation

---
+++ models/arima.py
@@ -0,0 +1,11 @@
+from statsmodels.tsa.arima_model import ARIMA
+
+class ARIMAModel(BaseModel):
+    def __init__(self, order):
+        self.model = ARIMA(order=order)
+
+    def train(self, X_train, y_train):
+        self.model.fit(X_train, y_train)
+
+    def predict(self, X_test):
+        return self.model.predict(X_test)
