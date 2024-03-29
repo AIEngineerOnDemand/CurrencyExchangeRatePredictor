7a9f24277bff62b7f07b37ea05d34d67422c7294
AIEngineerOnDemand <giopantana@gmail.com>
2024-03-29 15:01:06 -0500

    Update get_exchange_rate_history function and add plot_predictions function

--- utils/utils.py
+++ utils/utils.py
@@ -1,18 +1,39 @@
-from forex_python.converter import CurrencyRates
+#from forex_python.converter import CurrencyRates
+import os
+import yfinance as yf
 import pandas as pd
+import matplotlib.pyplot as plt
+from datetime import timedelta
 
-def get_exchange_rate_history(base_currency, target_currency, start_date, end_date):
-    currency_rates = CurrencyRates()
+def get_exchange_rate_history(from_currency, to_currency, start_date, end_date):
+    # Get the directory of the current script
+    dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
-    date_range = pd.date_range(start=start_date, end=end_date)
-    exchange_rates = []
+    # Define the file path
+    file_path = os.path.join(dir_path, f'data/{from_currency}_{to_currency}_{start_date}_{end_date}.csv')
 
-    for date in date_range:
-        try:
-            rate = currency_rates.get_rate(base_currency, target_currency, date)
-            exchange_rates.append((date, rate))
-        except:
-            print(f"Could not get rate for {date}")
+    # Check if the file exists
+    if os.path.exists(file_path):
+        # If the file exists, read it
+        data = pd.read_csv(file_path)
+    else:
+        # If the file does not exist, download the data
+        ticker_symbol = f"{from_currency}{to_currency}=X"
+        data = yf.download(ticker_symbol, start=start_date, end=end_date)
 
-    return pd.DataFrame(exchange_rates, columns=['Date', 'Rate'])
+        # Save the data to a CSV file
+        data.to_csv(file_path)
 
+    return data
+
+def plot_predictions(actual_data, predicted_data, title, filename):
+    plt.figure(figsize=(10, 6))
+    plt.plot(actual_data['Date'], actual_data['Rate'], color='black', label='Actual')
+    plt.plot(predicted_data['Date'], predicted_data['Rate'], color='red', label='Predicted')
+    plt.xlabel('Date')
+    plt.ylabel('Exchange Rate')
+    plt.title(title)
+    plt.legend()
+
+    # Save the plot as a PNG file
+    plt.savefig(filename)
