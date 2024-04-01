41d1ff42cd5aeee5e01751092b412564640bb1e1
AIEngineerOnDemand <giopantana@gmail.com>
2024-04-01 11:36:55 -0500

    Refactor main function to use YAML parameters and split data

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
-import cProfile
-import warnings
-from statsmodels.tools.sm_exceptions import ValueWarning
-warnings.simplefilter('ignore', ValueWarning)
+import yaml
 from datetime import datetime, timedelta
-from utils.utils import get_exchange_rate_history
+from utils.utils import get_exchange_rate_history, split_data, parse_common_args
 from models.arima import ARIMAModel
 from utils.utils import plot_predictions 
 import pandas as pd
 
 def main():
-    # Get USD to COP exchange rates for the past 2 years
-    start_date, end_date = '2023-01-01', '2024-03-28'
-    usd_cop = get_exchange_rate_history('USD', 'COP', start_date, end_date)
+    
+    args = parse_common_args()
+    
+    # Load parameters from YAML file
+    with open('params.yml', 'r') as file:
+        params = yaml.safe_load(file)
+
+    # Define the ARIMA model
+    model = ARIMAModel(order=tuple(params['model_order']))
+
+    # Load the ARIMA model from the file
+    model_name = f"ARIMA_{model.order}_{params['train_start_date']}_to_{params['train_end_date']}_model.pkl"
+    model.load(model_name)
+
+    # Get USD to COP exchange rates for the specified dates
+    usd_cop = get_exchange_rate_history('USD', 'COP', params['extract_start_date'], params['extract_end_date'])
 
     # Use only the 'High' column
     data = usd_cop[['High']]
 
     # Split the data into training and testing sets
-    train = data[data.index.year < 2024]
-    test = data[(data.index.year == 2024) & (data.index.month == 1) & (data.index.day <= 7)]
-
-    # Define the ARIMA model
-    model = ARIMAModel(order=(1, 1, 1))  # Adjust the order parameters as needed
-
-    # Train the model on the USD to COP exchange rate
-    model.train(train)
-    print('finished ')
+    train, test = split_data(data, args.train_start_date, args.train_end_date)
 
     # Make predictions for the test data
     predictions = model.predict(test.index[0], test.index[-1])
@@ -36,10 +38,11 @@ def main():
         'Rate': predictions
     })
 
-    # Filter the training data to only include the last month of 2023
-    train_last_month = train[(train.index.year == 2023) & (train.index.month == 12)]
+    # Filter the training data to only include the last month of the training period
+    train_last_month = train[(train.index.year == int(params['train_end_date'][:4])) & (train.index.month == int(params['train_end_date'][5:7]))]
 
     # Plot the actual and predicted exchange rates
     plot_predictions(train_last_month, test, predicted_data, 'USD to COP Exchange Rate', 'usd_cop_predictions.png')
 
-cProfile.run('main()')
+if __name__ == "__main__":
+    main()
