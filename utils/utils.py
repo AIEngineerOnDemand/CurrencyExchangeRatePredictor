0af933ed7759ad5939b72a337ed249be283c4329
AIEngineerOnDemand <giopantana@gmail.com>
2024-04-01 11:36:44 -0500

    Add argparse and yaml support, and implement data splitting function

--- utils/utils.py
+++ utils/utils.py
@@ -3,7 +3,23 @@ import os
 import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
-from datetime import timedelta
+import argparse
+import yaml
+
+def parse_common_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--extract_start_date', type=str)
+    parser.add_argument('--extract_end_date', type=str)
+    parser.add_argument('--train_start_date', type=str)
+    parser.add_argument('--train_end_date', type=str)
+    parser.add_argument('--model_order', type=str)  # Pass as a string, e.g., "[1, 1, 1]"
+    args = parser.parse_args()
+
+    # Convert model_order from string to list of integers
+    args.model_order = [int(x) for x in yaml.safe_load(args.model_order)]
+
+    return args
+
 
 def get_exchange_rate_history(from_currency, to_currency, start_date, end_date):
     # Get the directory of the current script
@@ -25,6 +41,12 @@ def get_exchange_rate_history(from_currency, to_currency, start_date, end_date):
         data.to_csv(file_path)
 
     return data
+
+def split_data(data, start_date, end_date):
+    train = data[(data.index >= start_date) & (data.index <= end_date)]
+    test = data[data.index > end_date]
+    return train, test
+
 def plot_predictions(train, test, predicted_data, title, filename):
     plt.figure(figsize=(10, 5))
     plt.plot(train.index, train['High'], color='black', label='Train')
