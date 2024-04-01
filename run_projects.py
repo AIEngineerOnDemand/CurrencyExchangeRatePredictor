bcd67c4fa95baa08e0d43350a17377cf93bf4af1
AIEngineerOnDemand <giopantana@gmail.com>
2024-04-01 11:50:15 -0500

    Fix missing closing brace in run_projects.py

--- run_projects.py
+++ run_projects.py
@@ -12,7 +12,8 @@ mlflow.run("train_model.py", parameters=params)
 infer_params = {
     'model_name': f"ARIMA_{tuple(params['model_order'])}_{params['train_start_date']}_to_{params['train_end_date']}_model.pkl",
     'start_date': params['extract_end_date'],
-    'end_date': params['extract_end_date']  
+    'end_date': params['extract_end_date']
+}  # Added closing brace here
 
 # Run the inference script as an MLflow project
 mlflow.run("main.py", parameters=infer_params)
