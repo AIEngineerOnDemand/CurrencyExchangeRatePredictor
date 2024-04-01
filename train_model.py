e0abec04623a8aefb39cba6ab6eed4c713bdc283
AIEngineerOnDemand <giopantana@gmail.com>
2024-04-01 11:36:50 -0500

    Refactor train_model.py to use new utility functions and save model using model's save method

--- train_model.py
+++ train_model.py
@@ -1,17 +1,8 @@
-import argparse
-import yaml
 from models.arima import ARIMAModel
-from utils.utils import get_exchange_rate_history
-import joblib
+from utils.utils import get_exchange_rate_history,split_data,parse_common_args
 
 # Parse command line arguments
-parser = argparse.ArgumentParser()
-parser.add_argument('--extract_start_date', type=str)
-parser.add_argument('--extract_end_date', type=str)
-parser.add_argument('--train_start_date', type=str)
-parser.add_argument('--train_end_date', type=str)
-parser.add_argument('--model_order', type=str)  # Pass as a string, e.g., "[1, 1, 1]"
-args = parser.parse_args()
+args = parse_common_args()
 
 # Convert model_order from string to list of integers
 model_order = [int(x) for x in yaml.safe_load(args.model_order)]
@@ -23,8 +14,7 @@ usd_cop = get_exchange_rate_history('USD', 'COP', args.extract_start_date, args.
 data = usd_cop[['High']]
 
 # Split the data into training and testing sets
-train = data[(data.index >= args.train_start_date) & (data.index <= args.train_end_date)]
-test = data[data.index > args.train_end_date]
+train, test = split_data(data, args.train_start_date, args.train_end_date)
 
 # Define the ARIMA model
 model = ARIMAModel(order=tuple(model_order))  # Convert list to tuple
@@ -36,4 +26,4 @@ model.train(train)
 model_name = f"ARIMA_{model.order}_{args.train_start_date}_to_{args.train_end_date}_model.pkl"
 
 # Save the trained model to a file
-joblib.dump(model, model_name)
+model.save(model_name)
