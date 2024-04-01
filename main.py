223182f441ea4b6936a1f4df7665d8a3692da772
AIEngineerOnDemand <giopantana@gmail.com>
2024-04-01 11:50:27 -0500

    Refactor main.py to improve code structure and readability

--- main.py
+++ main.py
@@ -1,27 +1,20 @@
-import yaml
-from datetime import datetime, timedelta
-from utils.utils import get_exchange_rate_history, split_data, parse_common_args
+from utils.utils import plot_predictions, parse_common_args, split_data
 from models.arima import ARIMAModel
-from utils.utils import plot_predictions 
+from utils.utils import get_exchange_rate_history
 import pandas as pd
 
 def main():
-    
+    # Parse command line arguments
     args = parse_common_args()
-    
-    # Load parameters from YAML file
-    with open('params.yml', 'r') as file:
-        params = yaml.safe_load(file)
 
     # Define the ARIMA model
-    model = ARIMAModel(order=tuple(params['model_order']))
+    model = ARIMAModel(order=args.model_order)
 
     # Load the ARIMA model from the file
-    model_name = f"ARIMA_{model.order}_{params['train_start_date']}_to_{params['train_end_date']}_model.pkl"
-    model.load(model_name)
+    model.load(args.model_name)
 
     # Get USD to COP exchange rates for the specified dates
-    usd_cop = get_exchange_rate_history('USD', 'COP', params['extract_start_date'], params['extract_end_date'])
+    usd_cop = get_exchange_rate_history('USD', 'COP', args.extract_start_date, args.extract_end_date)
 
     # Use only the 'High' column
     data = usd_cop[['High']]
@@ -39,10 +32,11 @@ def main():
     })
 
     # Filter the training data to only include the last month of the training period
-    train_last_month = train[(train.index.year == int(params['train_end_date'][:4])) & (train.index.month == int(params['train_end_date'][5:7]))]
+    train_last_month = train[(train.index.year == int(args.train_end_date[:4])) & (train.index.month == int(args.train_end_date[5:7]))]
 
     # Plot the actual and predicted exchange rates
-    plot_predictions(train_last_month, test, predicted_data, 'USD to COP Exchange Rate', 'usd_cop_predictions.png')
+    # Plot the actual and predicted exchange rates
+    plot_predictions(train_last_month, test, predicted_data, 'USD to COP Exchange Rate', 'results/usd_cop_predictions.png')
 
 if __name__ == "__main__":
     main()
