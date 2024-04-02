5a179e31b9d7b9abecdf347f2ae1922f87c1e5e1
AIEngineerOnDemand <giopantana@gmail.com>
2024-04-02 13:11:37 -0500

    Refactor train_model.py to improve code structure and readability

--- train_model.py
+++ train_model.py
@@ -1,30 +1,31 @@
 from models.arima import ARIMAModel
-from utils.utils import get_exchange_rate_history, split_data, parse_common_args
+from utils.utils import get_exchange_rate_history, split_data
 import yaml
 
-# Parse command line arguments
-args = parse_common_args()
+def train_model(model_order, train_start_date, train_end_date, extract_end_date):
+    # Convert model_order from string to list of integers
+    model_order = [int(x) for x in yaml.safe_load(model_order)]
 
-# Convert model_order from string to list of integers
-model_order = [int(x) for x in yaml.safe_load(args.model_order)]
+    # Get USD to COP exchange rate history
+    usd_cop = get_exchange_rate_history('USD', 'COP', train_start_date, extract_end_date)
 
-# Get USD to COP exchange rate history
-usd_cop = get_exchange_rate_history('USD', 'COP', args.extract_start_date, args.extract_end_date)
+    # Use only the 'High' column
+    data = usd_cop[['High']]
 
-# Use only the 'High' column
-data = usd_cop[['High']]
+    # Split the data into training and testing sets
+    train, test = split_data(data, train_start_date, train_end_date)
 
-# Split the data into training and testing sets
-train, test = split_data(data, args.train_start_date, args.train_end_date)
+    # Define the ARIMA model
+    model = ARIMAModel(order=tuple(model_order))  # Convert list to tuple
 
-# Define the ARIMA model
-model = ARIMAModel(order=tuple(model_order))  # Convert list to tuple
+    # Train the model on the USD to COP exchange rate
+    model.train(train)
 
-# Train the model on the USD to COP exchange rate
-model.train(train)
+    # Create a filename that includes the model's name, order parameters, start date, and end date
+    model_name = f"ARIMA_{model.order}_{train_start_date}_to_{train_end_date}_model.pkl"
 
-# Create a filename that includes the model's name, order parameters, start date, and end date
-model_name = f"ARIMA_{model.order}_{args.train_start_date}_to_{args.train_end_date}_model.pkl"
-
-# Save the trained model to a file
-model.save(model_name)
+    # Save the trained model to a file
+    model.save(model_name)
+    
+    # Return the model name
+    return model_name
